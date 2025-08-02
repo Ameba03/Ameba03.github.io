a9c1b24d3fd8834ee9746250adbb7083a122600d
Ameba03 <contactoalberrr13@gmail.com>
2025-08-02 11:59:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 function siguientePantalla(id) {
-  document.querySelectorAll('.pantalla').forEach(p => {
-    p.classList.remove('activa');
-    p.classList.add('oculto');
-  });
+  const pantallas = document.querySelectorAll('.pantalla');
+  pantallas.forEach(p => p.classList.remove('visible'));
+  pantallas.forEach(p => p.classList.add('oculto'));
+
   const siguiente = document.getElementById(id);
   if (siguiente) {
     siguiente.classList.remove('oculto');
-    siguiente.classList.add('activa');
+    siguiente.classList.add('visible');
   }
 }
 
-function responder(respuesta) {
-  const div = document.getElementById('respuesta');
-  if (respuesta === 'si') {
-    div.innerHTML = '🚀 ¡BOOOM! Allá vamos ✨';
-  } else {
-    div.innerHTML = 'Uuups... vuelve a pensarlo 🤔';
-  }
+function finalizar() {
+  siguientePantalla('pantallaFinal');
 }

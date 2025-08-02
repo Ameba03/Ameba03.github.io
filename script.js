17570fa1b9ca42c4947e31f7c6b2e2ba697b3a78
Ameba03 <contactoalberrr13@gmail.com>
2025-08-02 12:06:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 function siguientePantalla(id) {
-    const pantallas = document.querySelectorAll('.pantalla');
-    pantallas.forEach(p => {
-        p.classList.remove('visible');
-        p.classList.add('oculto');
-    });
+  const pantallas = document.querySelectorAll('.pantalla');
+  pantallas.forEach(p => {
+    p.classList.remove('visible');
+    p.classList.add('oculto');
+  });
 
-    const siguiente = document.getElementById(id);
-    if (siguiente) {
-        siguiente.classList.remove('oculto');
-        siguiente.classList.add('visible');
-    }
+  const siguiente = document.getElementById(id);
+  if (siguiente) {
+    siguiente.classList.remove('oculto');
+    siguiente.classList.add('visible');
+  }
+}
+
+function finalizar() {
+  siguientePantalla('pantallaFinal');
 }
